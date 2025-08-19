058387714361b2f608052d54672cac6fc649dacb
Thauã Mello <103223038+ThauaMello@users.noreply.github.com>
2025-08-19 12:28:35 -0300

    Fila estatica
    em dev

---
+++ FilaEstatica/FilaEstatica/Fila.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilaEstatica
+{
+    
+    internal class Fila
+    {
+
+        string[] fila;
+        int primeiro;
+        int ultimo;
+
+        public Fila(int Tamanho)
+        {
+            fila = new string[Tamanho];
+            primeiro = 0;
+            ultimo = 0;
+        }
+
+        public void Enfileirar(String valor)
+        {
+            if (ultimo < fila.Length)
+            {
+                fila[ultimo] = valor;
+                ultimo++;
+            }
+            else
+            {
+                Console.WriteLine("Valor Cheia!");
+            }
+        }
+        public void Imprimir()
+            {
+                for (int i = 0; i < fila.Length; i++)
+                {
+                Console.WriteLine(fila[i]);
+                }
+            }
+    }
+}
---
+++ FilaEstatica/FilaEstatica/Program.cs
@@ -0,0 +1,14 @@
+﻿using FilaEstatica;
+Fila agenda = new Fila(5);
+
+
+Console.WriteLine("Trabalhando com a Fila");
+Console.WriteLine();
+
+agenda.Enfileirar("A");
+agenda.Enfileirar("B");
+agenda.Enfileirar("C");
+agenda.Enfileirar("D");
+agenda.Enfileirar("E");
+
+agenda.Imprimir();
