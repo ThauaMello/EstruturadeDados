0b87ea595a5ba5f3adfa11fa0fff06a6cf01e490
Thauã Mello <103223038+ThauaMello@users.noreply.github.com>
2025-09-02 11:40:33 -0300

    Arquivo de FIla Circular
    Trabalho desenvolvido em aula - fila circular

---
+++ FilaCircular/FilaCircular/Fila.cs
@@ -0,0 +1,72 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilaCircular
+{
+    internal class Fila
+    {
+        string[] valores;
+        int inicio, fim, total;
+
+        public Fila(int Tamanho)
+        {
+            valores = new string[Tamanho];
+            inicio = 0;
+            fim = -1;
+            total = 0;
+        }
+
+        public void Enfileirar(string Valor)
+        {
+            if (total < valores.Length){
+                fim = (fim + 1) % valores.Length;
+                valores[fim] = Valor;  
+                total++;
+            }
+            else
+            {
+                Console.WriteLine("Fila está cheia");
+                Console.ReadKey();
+            }
+        }
+
+        public string Desenfileirar()
+        {
+            if (total > 0)
+            {
+                string valor = valores[inicio];
+                valores[inicio] = string.Empty;
+                inicio = (inicio + 1) % valores.Length;
+                total--;
+                return valor;
+            }
+            else
+            {
+                Console.WriteLine("Fila está vazia!");
+                Console.ReadKey();
+                return null;
+            }
+        }
+
+
+        public void Imprimir()
+        {
+            if (total == 0)
+            {
+                Console.WriteLine("Fila vazia!");
+                return;
+            }
+
+            int i = inicio;
+            for (int count = 0; count < total; count++)
+            {
+                Console.WriteLine(valores[i]);
+                i = (i + 1) % valores.Length;
+            }
+        }
+
+    }
+}
---
+++ FilaCircular/FilaCircular/Program.cs
@@ -0,0 +1,90 @@
+﻿using FilaCircular;
+
+
+string opcao = ""; // variável que irá pegar a opção que o usuário desejar 
+string valor; // valor que o usuário irá inserir na fila circular
+Fila fila = new Fila(5); // definindo o tamanho da fila em cinco
+
+Console.WriteLine("Sistema em C# para testar a execução de uma fila circular\n");
+
+do
+{
+    try
+    {
+        Console.Write("\nDigite: \n\n   1-> Enfileirar \n\n   2-> Desenfileirar " +
+        "\n\n   3-> Imprimir \n\n   9-> Sair \n\n   Sua opção --> ");
+        opcao = Console.ReadLine();
+        switch (opcao)
+        {
+            case "1":
+                Console.Write("Digite um valor para enfileirar: ");
+                valor = Console.ReadLine();
+                fila.Enfileirar(valor);
+                break;
+            case "2":
+                Console.WriteLine("Desenfileirado: {0}");
+                fila.Desenfileirar();
+                break;
+            case "3":
+                Console.WriteLine("Impressão da fila");
+                fila.Imprimir();
+                Console.ReadKey();
+                break;
+            case "9":
+                Console.WriteLine("Saindo do sistema...");
+                break;
+            default:
+                Console.WriteLine("Opção inválida!!!");
+                break;
+        }
+    }
+    catch (Exception erro)
+    {
+        Console.WriteLine(erro.Message);
+    }
+    Console.Clear();
+} while (opcao != "9");
+
+
+// VERSÃO USANDO O COMANDO SE
+//string opcao = ""; // variável que irá pegar a opção que o usuário desejar 
+//string valor; // valor que o usuário irá inserir na fila circular
+//Fila fila = new Fila(5); // definindo o tamanho da fila em cinco
+
+//Console.WriteLine("Sistema em C# para testar a execução de uma fila circular.\n");
+
+
+//while (opcao != "9")
+//{
+//    Console.Write("\nDigite: \n\n   1-> Enfileirar \n\n   2-> Desenfileirar " +
+//    "\n\n   3-> Imprimir \n\n   9-> Sair \n\n   Sua opção --> ");
+//    opcao = Console.ReadLine();
+
+//    if (opcao == "1")
+//    {
+//        Console.Write("Digite um valor para enfileirar: ");
+//        valor = Console.ReadLine();
+//        fila.Enfileirar(valor);
+//    }
+
+//    if (opcao == "2")
+//    {
+//        Console.WriteLine("Desenfileirado:");
+//        fila.Desenfileirar();
+//    }
+
+//    if (opcao == "3")
+//    {
+//        Console.WriteLine("Impressão da fila");
+//        fila.Imprimir();
+//        Console.WriteLine("Pressione uma tecla...");
+//        Console.ReadKey();
+//    }
+
+//    if (opcao == "9")
+//    {
+//        Console.WriteLine("Saindo do sistema...");
+//    }
+
+//    Console.Clear();
+//};
